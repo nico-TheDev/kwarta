54ba226f07676c5c3b3a1ebcf476d30435d14c65
Nico Ignacio <norbertoignaciojr@gmail.com>
2023-03-02 18:39:04 +0800

    feat: custom icon component

--- src/pages/categories.js
+++ src/pages/categories.js
@@ -1,9 +1,11 @@
-import Head from 'next/head'
-import { Box, Container, Grid, Pagination } from '@mui/material'
-import { accounts } from '../__mocks__/accounts'
-import { CategoriesHead } from '../components/categories/categories-head'
-import { CategoriesCard } from '../components/categories/categories-card'
-import { DashboardLayout } from '../components/dashboard-layout'
+import Head from 'next/head';
+import { Box, Container, Grid, Pagination } from '@mui/material';
+import { accounts } from '../__mocks__/accounts';
+import { CategoriesHead } from '../components/categories/categories-head';
+import { CategoriesCard } from '../components/categories/categories-card';
+import { DashboardLayout } from '../components/dashboard-layout';
+import { Icon } from 'components/shared/Icon';
+import { ICON_NAMES } from 'constants/constant';
 
 const Page = () => (
     <>
@@ -24,6 +26,7 @@ const Page = () => (
                         {accounts.map((account) => (
                             <Grid item key={account.id} lg={4} md={6} xs={12}>
                                 <CategoriesCard account={account} />
+                                <Icon name={ICON_NAMES.SYSTEM_ICONS.ACCOUNTS} color='primary' fontSize='large' />
                             </Grid>
                         ))}
                     </Grid>
@@ -40,8 +43,8 @@ const Page = () => (
             </Container>
         </Box>
     </>
-)
+);
 
-Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>
+Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
-export default Page
+export default Page;
