73ef9400b6d12f9293bacfcf12f05c1cc621d145
xcboi2k <darkspikyshark@gmail.com>
2023-03-10 16:19:18 +0800

    feat: default log in and sign up

--- src/stores/useAuthStore.js
+++ src/stores/useAuthStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 
-import { signInWithPopup, GoogleAuthProvider, getAdditionalUserInfo } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, signInWithPopup, GoogleAuthProvider, getAdditionalUserInfo } from 'firebase/auth'
 import { auth } from '../../firebase.config'
 
 const AuthStore = (set, get) => ({
@@ -9,6 +9,61 @@ const AuthStore = (set, get) => ({
         user: null,
         isAuthenticated: false,
         isLoading: false
+    },
+    addUser: async (newUser) => {
+        try {
+            const createdUserResponse = await createUserWithEmailAndPassword(auth, newUser.email, newUser.password);   //creates user
+            await updateProfile(auth.currentUser, {
+                displayName: newUser.firstName + " " + newUser.lastName,    //updates displayName
+                photoURL: newUser.profile_img,                             //updates photoURL
+            });
+            await setDoc(doc(db, "users", createdUserResponse.user.uid), {      //sets document of user
+                uid: createdUserResponse.user.uid,                              //generated uid
+                first_name: newUser.firstName,                  //fetched data from firstName (RegisterScreen) will be stored here
+                last_name: newUser.lastName,                    //fetched data from lastName (RegisterScreen) will be stored here
+                email: newUser.email,                           //fetched data from email (RegisterScreen) will be stored here
+                profile_img: newUser.profile_img                //fetched data from profile_img (RegisterScreen) will be stored here
+            });
+            set({
+                authState: {
+                    user: {
+                        name: createdUserResponse.user.displayName,
+                        firstName: newUser.firstName,
+                        lastName: newUser.lastName,
+                        email: createdUserResponse.user.email,
+                        photo: createdUserResponse.user.photoURL,
+                        uid: createdUserResponse.user.uid,
+                    },
+                    isAuthenticated: true,
+                    isLoading: false
+                }
+            })
+        }
+        catch (err) {
+            console.log(err);
+        }
+    },
+    verifyUser: async (login_user) => {
+        try {
+            const verifiedResponse = await signInWithEmailAndPassword(auth, login_user.email, login_user.password);     //checks if user is registered, email and password correct
+            const verifiedUser = verifiedResponse.user;
+            // console.log(verifiedResponse);
+            set({
+                authState: {
+                    user: {
+                        name: verifiedUser.displayName,
+                        email: verifiedUser.email,
+                        photo: verifiedUser.photoURL,
+                        uid: verifiedUser.uid,
+                    },
+                    isAuthenticated: true,
+                    isLoading: false
+                }
+            })
+        }
+        catch (err) {
+            console.log(err.message);
+        }
     },
     loginWithGoogle: async () => {
         try {
