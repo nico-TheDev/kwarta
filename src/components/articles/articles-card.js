8fc916a41d143c6fd15385ee83b76eb3616fe0fb
Nico Ignacio <norbertoignaciojr@gmail.com>
2023-03-28 00:27:31 +0800

    feat: sort articles

--- src/components/articles/articles-card.js
+++ src/components/articles/articles-card.js
@@ -1,22 +1,39 @@
 import {useEffect, useState} from 'react';
-import { Box, Button, Card, CardContent, TextField, InputAdornment, SvgIcon, Typography, Link, CardMedia} from '@mui/material'
-import { getLanguage } from 'utils/getLanguage'
-import Image from 'next/image'
+import {
+    Box,
+    Button,
+    Card,
+    CardContent,
+    TextField,
+    Stack,
+    Chip,
+    SvgIcon,
+    Typography,
+    Link,
+    CardMedia
+} from '@mui/material';
+import { getLanguage } from 'utils/getLanguage';
+import Image from 'next/image';
 
 export const ArticlesCard = ({ articles, ...rest }) => {
-    return(
-        <Link href={articles.articleLink} underline="none" target="_blank">
+    return (
+        <Link href={articles.articleLink} underline='none' target='_blank'>
             <Card sx={{ height: '100%' }} {...rest}>
                 <CardContent>
                     <CardMedia
-                        sx={{ height: 140 }}
+                        sx={{ height: 140, mb: 2 }}
                         image={articles.articleImage}
                         title={articles.articleTitle}
                     />
                     <Box>
-                        <Typography color='textPrimary' variant='h5'>
+                        <Typography color='textPrimary' variant='h6' mb={4}>
                             {articles.articleTitle}
                         </Typography>
+                        <Stack direction='row' spacing={1} mb={2}>
+                            {articles.articleTags.map((item) => (
+                                <Chip label={item} variant='outlined' color='secondary' />
+                            ))}
+                        </Stack>
                         <Typography color='textSecondary' variant='subtitle1'>
                             {articles.articleAuthor}
                         </Typography>
@@ -24,5 +41,5 @@ export const ArticlesCard = ({ articles, ...rest }) => {
                 </CardContent>
             </Card>
         </Link>
-    )
-}
+    );
+};
--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,85 +1,43 @@
 import {useEffect, useState} from 'react';
 import Head from 'next/head'
-import { Box, Container, Grid, Pagination, Stack} from '@mui/material'
+import { Box, Container, Grid, Pagination, Stack, Typography, Checkbox } from '@mui/material';
 import Chip from '@mui/material/Chip';
-import CheckIcon from '@mui/icons-material/Check';
-import articles from '../data/articles'
-import { ArticlesHead } from '../components/articles/articles-head'
-import { ArticlesCard } from '../components/articles/articles-card'
-import { DashboardLayout } from '../components/dashboard-layout'
+import SearchOffIcon from '@mui/icons-material/SearchOff';
+import articles from '../data/articles';
+import { getLanguage } from 'utils/getLanguage';
+import { ArticlesCard } from '../components/articles/articles-card';
+import { DashboardLayout } from '../components/dashboard-layout';
+
+const filters = ['General', 'Savings', 'Stocks', 'Investments', 'Bonds'];
 
 const Page = () => {
     const [filterValue, setFilterValue] = useState([]);
     const [articleList, setArticleList] = useState(articles);
 
-    const generalArticles = articles.filter(articles => articles.articleTags.includes('General'));
-    const savingsArticles = articles.filter(articles => articles.articleTags.includes('Savings'));
-    const stocksArticles = articles.filter(articles => articles.articleTags.includes('Stocks'));
-    const investmentsArticles = articles.filter(articles => articles.articleTags.includes('Investments'));
-    const bondsArticles = articles.filter(articles => articles.articleTags.includes('Bonds'));
+    const handleClick = (value) => {
+        const copy = [...filterValue];
+        if (!filterValue.includes(value)) {
+            copy.push(value);
+        } else {
+            const index = copy.findIndex((item) => item === value);
+            copy.splice(index, 1);
+        }
+        setFilterValue(copy);
+    };
 
     useEffect(() => {
-        if (filterValue.includes('')) setArticleList(articles);
-        else if (filterValue.includes('General')) setArticleList(generalArticles);
-        else if (filterValue.includes('Savings')) setArticleList(savingsArticles);
-        else if (filterValue.includes('Stocks')) setArticleList(stocksArticles);
-        else if (filterValue.includes('Investments')) setArticleList(investmentsArticles);
-        else if (filterValue.includes('Bonds')) setArticleList(bondsArticles);
-    }, [articleList]);
+        let checkSubset = (parentArray, subsetArray) => {
+            let set = new Set(parentArray);
+            return subsetArray.every((x) => set.has(x));
+        };
 
-    const handleGeneralClick = () => {
-        if (!filterValue.includes('General')){
-            setFilterValue(filterValue => [...filterValue, 'General']);
+        if (filterValue.length === 0) setArticleList(articles);
+        else {
+            setArticleList(articles.filter((item) => checkSubset(item.articleTags, filterValue)));
         }
-    };
-    const handleSavingsClick = () => {
-        if (!filterValue.includes('Savings')){
-            setFilterValue(filterValue => [...filterValue, 'Savings']);
-        }
-    };
-    const handleStocksClick = () => {
-        if (!filterValue.includes('Stocks')){
-            setFilterValue(filterValue => [...filterValue, 'Stocks']);
-        }
-    };
-    const handleInvestmentsClick = () => {
-        if (!filterValue.includes('Investments')){
-            setFilterValue(filterValue => [...filterValue, 'Investments']);
-        }
-    };
-    const handleBondsClick = () => {
-        if (!filterValue.includes('Bonds')){
-            setFilterValue(filterValue => [...filterValue, 'Bonds']);
-        }
-    };
+    }, [filterValue]);
 
-    const handleGeneralRemove = () => {
-        if (filterValue.includes('General')){
-            setFilterValue(filterValue.filter(item => item !== 'General'));
-        }
-    };
-    const handleSavingsRemove = () => {
-        if (filterValue.includes('Savings')){
-            setFilterValue(filterValue.filter(item => item !== 'Savings'));
-        }
-    };
-    const handleStocksRemove = () => {
-        if (filterValue.includes('Stocks')){
-            setFilterValue(filterValue.filter(item => item !== 'Stocks'));
-        }
-    };
-    const handleInvestmentsRemove = () => {
-        if (filterValue.includes('Investments')){
-            setFilterValue(filterValue.filter(item => item !== 'Investments'));
-        }
-    };
-    const handleBondsRemove = () => {
-        if (filterValue.includes('Bonds')){
-            setFilterValue(filterValue.filter(item => item !== 'Bonds'));
-        }
-    };
-
-    return(
+    return (
         <>
             <Head>
                 <title>Articles | CASH</title>
@@ -92,30 +50,54 @@ const Page = () => {
                 }}
             >
                 <Container maxWidth={false}>
-                    <ArticlesHead />
+                    <Box
+                        sx={{
+                            alignItems: 'center',
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            flexWrap: 'wrap',
+                            m: -1
+                        }}
+                    >
+                        <Typography sx={{ m: 1 }} variant='h4'>
+                            {getLanguage().articles}
+                        </Typography>
+                    </Box>
                     <Box sx={{ pt: 3 }}>
-                        <Stack direction="row" spacing={1}>
-                            <Chip label="General" onClick={handleGeneralClick} onDelete={handleGeneralRemove}/>
-                            <Chip label="Savings" onClick={handleSavingsClick} onDelete={handleSavingsRemove}/>
-                            <Chip label="Stocks" onClick={handleStocksClick} onDelete={handleStocksRemove}/>
-                            <Chip label="Investments" onClick={handleInvestmentsClick} onDelete={handleInvestmentsRemove}/>
-                            <Chip label="Bonds" onClick={handleBondsClick} onDelete={handleBondsRemove}/>
+                        <Stack direction='row' spacing={1}>
+                            {filters.map((item) => (
+                                <Chip
+                                    label={item}
+                                    onClick={() => handleClick(item)}
+                                    variant={filterValue.includes(item) ? 'filled' : 'outlined'}
+                                    color='primary'
+                                />
+                            ))}
                         </Stack>
                     </Box>
                     <Box sx={{ pt: 3 }}>
                         <Grid container spacing={3}>
-                            {articles.map((articles) => (
-                                <Grid item key={articles.id} lg={4} md={6} xs={12}>
-                                    <ArticlesCard articles={articles} />
+                            {articleList.length !== 0 ? (
+                                articleList.map((articles) => (
+                                    <Grid item key={articles.id} lg={4} md={6} xs={12}>
+                                        <ArticlesCard articles={articles} />
+                                    </Grid>
+                                ))
+                            ) : (
+                                <Grid item xs={12} sx={{ p: 3, textAlign: 'center' }}>
+                                    <Box sx={{ fontSize: 100 }}>
+                                        <SearchOffIcon fontSize='inherit' />
+                                    </Box>
+                                    <Typography variant='h5'>No Articles Found</Typography>
                                 </Grid>
-                            ))}
+                            )}
                         </Grid>
                     </Box>
                 </Container>
             </Box>
         </>
-    )
-}
+    );
+};
 
 Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>
 
