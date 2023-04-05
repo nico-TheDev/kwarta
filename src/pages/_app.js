58a0eceedb96c83aca5f7d98a7184943ed699081
Nico Ignacio <norbertoignaciojr@gmail.com>
2023-04-06 03:23:57 +0800

    feat: pwa initial

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -53,8 +53,22 @@ const App = (props) => {
     return (
         <CacheProvider value={emotionCache}>
             <Head>
+                <meta charSet='utf-8' />
+                <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+                <meta
+                    name='viewport'
+                    content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
+                />
+                <meta name='description' content='Description' />
+                <meta name='keywords' content='Keywords' />
                 <title>CASH: Financial Monitoring System</title>
                 <meta name='viewport' content='initial-scale=1, width=device-width' />
+
+                <link rel='manifest' href='/manifest.json' />
+                <link href='/icons/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+                <link href='/icons/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+                <link rel='apple-touch-icon' href='/apple-icon.png'></link>
+                <meta name='theme-color' content='#317EFB' />
             </Head>
             <LocalizationProvider dateAdapter={AdapterDateFns}>
                 <Toaster position='bottom-left' />
